621352acb1d9fcd6237cf4cee1a42404c1f3e6d1
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-03-10 01:17:11 -0300

    Criando loader de models

--- helper/db/index.js
+++ helper/db/index.js
@@ -1,18 +1,23 @@
 import Sequelize from 'sequelize';
 
+import databaseConfig from '../../config/database';
+import User from '../../model/User';
+import College from '../../model/College';
+
+const models = [User, College];
+
 class DataBase{
-  
     constructor(){
         this.init();
     }
 
     init(){
-        this.connection = new Sequelize(this.database, this.user, this.password, {
-            host: this.host,
-            dialect:'mysql'
-        });
-    }
+        this.connection = new Sequelize(databaseConfig);
 
+        models
+        .map(model => model.init(this.connection))
+        .map(model => model.associate && model.associate(this.connection.models))
+    }
 }
 
 export default new DataBase();
