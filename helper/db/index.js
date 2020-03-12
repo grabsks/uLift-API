6cd2c1ddba882e257b0202c989b5e55298745ddd
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-03-11 22:03:16 -0300

    Colocando array de models dentro da classe Database

--- helper/db/index.js
+++ helper/db/index.js
@@ -4,19 +4,18 @@ import databaseConfig from '../../config/database';
 import User from '../../model/User';
 import College from '../../model/College';
 
-const models = [User, College];
-
 class DataBase{
     constructor(){
+        this.models = [User, College]
         this.init();
     }
 
     init(){
         this.connection = new Sequelize(databaseConfig);
 
-        models
-        .map(model => model.init(this.connection))
-        .map(model => model.associate && model.associate(this.connection.models))
+        this.models         
+            .map(model => model.init(this.connection))
+            .map(model => model.associate && model.associate(this.connection.models))
     }
 }
 
