947ba3eb51e37ebc27d3ad5547b7cae2b52afc3f
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-03-18 19:19:45 -0300

    Criando migrations
    rodando o sequilize e criando as migrations iniciais

---
+++ migration/20200316035304-create-file-table.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      size: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      file_type: {
+        type: Sequelize.ENUM('ra', 'cnh', 'profile'),
+        allowNull: false,
+      },
+
+      body: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('files');
+  }
+};
