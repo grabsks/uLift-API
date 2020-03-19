6e68f0e08e41bc0f1e3405a4438585eb5e5eff7c
YuriAsthar <yuriasthar@hotmail.com>
2020-03-18 23:33:05 -0300

    coloca espaço nos attribute das migration

--- migration/20200316023952-create-college-table.js
+++ migration/20200316023952-create-college-table.js
@@ -6,30 +6,37 @@ module.exports = {
               primaryKey: true,
               allowNull: false,
           },
+
           name: {
               type:  Sequelize.STRING,
               allowNull: false,
           },
+
           street: {
               type: Sequelize.STRING,
               allowNull: false,
           },
+
           neighborhood: {
               type: Sequelize.STRING,
               allowNull: false,
           },
+
           city: {
               type: Sequelize.STRING,
               allowNull: false,
           },
+
           state: {
               type: Sequelize.STRING,
               allowNull: false,
           },
+
           created_at: {
               type: Sequelize.DATE,
               allowNull: false,
           },
+
           updated_at: {
               type: Sequelize.DATE,
               allowNull: false,
