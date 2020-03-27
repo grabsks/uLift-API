d6642ade2867c0a8a67f2b766dfe9ecee84b4a1e
luanfonsecap <luanfonsecap@icloud.com>
2020-03-27 11:24:07 -0300

    adicionado rota para buscar usuário

--- router/index.js
+++ router/index.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 
-import Hello from "../controller/hello";
 import UserController from "../controller/UserController";
 
 const routes = new Router();
 
 routes.post("/v1/users", UserController.register);
+routes.get("/v1/users/:id", UserController.search);
 
 export default routes;
