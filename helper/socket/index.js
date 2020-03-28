3cb6255567aa58eb10d713bcfa373062b39b5d2f
Hermes Francisco <franciscohermes2000@hotmail.com>
2020-03-28 15:10:07 -0300

    salvando progresso

--- helper/socket/index.js
+++ helper/socket/index.js
@@ -3,10 +3,19 @@ var user_id = [];
 
 io.on("connection", function(socket){
     socket.on("login", function(id, callback){
+        //socket = io('http://localhost:3000/');
+
         if(!(id in user_id)){
+
             socket.id = id;
             user_id[id] = socket;
+
+            io.sockets.emit("user_update", Object.keys(user_id));
+            
             callback(true);
-        }else callback(false);
+        }else{
+            callback(false);
+        }
+
     });
 });
