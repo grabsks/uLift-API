299971910ddc7c8da557f0ecd1e116afc0c61443
Hermes Francisco <franciscohermes2000@hotmail.com>
2020-03-29 18:08:45 -0300

    atualizando o arquivo do socket

--- helper/socket/index.js
+++ helper/socket/index.js
@@ -1,5 +1,7 @@
+import app from '../../app';
 var io = require('socket.io')(app);
 var user_id = [];
+console.log("test")
 
 io.on("connection", function(socket){
     socket.on("login", function(id, callback){
@@ -10,6 +12,8 @@ io.on("connection", function(socket){
             socket.id = id;
             user_id[id] = socket;
 
+            console.log(id);
+
             io.sockets.emit("user_update", Object.keys(user_id));
             
             callback(true);
@@ -18,4 +22,16 @@ io.on("connection", function(socket){
         }
 
     });
+    socket.on("send_message", function(message, callback){
+        socket.emit("chat_update", message);
+        user_id[message.To_id].emit("chat_update", message);
+        callback();        
+    });
+    socket.on("send_location", function(location, callback){
+        io.sockets.emit("location_update", location);
+        callback();
+    });
+    socket.on("lift_request", function(request, callback){
+        
+    })
 });
