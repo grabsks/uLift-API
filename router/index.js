f2541a2efd00369544f83d91ead40fe1a6adeedf
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-05-18 22:46:22 -0300

    autenticando usuário

--- router/index.js
+++ router/index.js
@@ -3,11 +3,14 @@ import { Router } from "express";
 import UserController from "../controller/UserController";
 import SessionController from "../controller/SessionController";
 
+import authMiddleware from '../middlewares/auth';
+
 const routes = new Router();
 
 routes.post("/v1/users", UserController.register);
 routes.post("/v1/sessions", SessionController.store);
 
+routes.use(authMiddleware);
 routes.get("/v1/users/:id", UserController.search);
 
 export default routes;
