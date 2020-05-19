c92cc6ba71a8919fe5d85a16857da4f51df05c22
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-05-19 15:47:30 -0300

    realizando verificação da senha

--- controller/UserController.js
+++ controller/UserController.js
@@ -6,8 +6,27 @@ class UserController {
       where: { email: request.body.email },
     });
 
-    if (userExists)
-      return response.status(200).json({ error: "Usuário já cadastrado" });
+    if (userExists) {
+      return response.status(200).json({ error: "usuário já cadastrado" });
+    }
+
+    if (
+      !/[a-z]/gm.test(request.body.password) ||
+      !/[A-Z]/gm.test(request.body.password) ||
+      !/[0-9]/gm.test(request.body.password) ||
+      !/[@!#$%&*()?:./^~_+-]/gm.test(request.body.password)
+    ) {
+      return response.status(400).json({
+        error:
+          "a senha deve conter no mínimo 1 letras maiúscula, 1 letra minúscula, 1 número e 1 caractere especial",
+      });
+    }
+
+    if (request.body.password.length < 8) {
+      return response
+        .status(400)
+        .json({ error: "a senha deve conter no mínimo 8 caracteres" });
+    }
 
     const { id, ra, name, email } = await User.create(request.body);
 
