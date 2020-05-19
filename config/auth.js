22ea1f5b620863752303fad9203a8e7f798d6f4e
GuilhermehChaves <contato.guilhermehchaves@gmail.com>
2020-05-18 21:14:57 -0300

    logando usuário

---
+++ config/auth.js
@@ -0,0 +1,4 @@
+export default  {
+    secret: '963159zadac435794e63d33bf6bc336e',
+    expiresIn: '7d',
+};
--- router/index.js
+++ router/index.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
 import UserController from "../controller/UserController";
+import SessionController from '../controller/SessionController';
 
 const routes = new Router();
 
 routes.post("/v1/users", UserController.register);
+routes.post("/v1/sessions", SessionController.store);
+
 routes.get("/v1/users/:id", UserController.search);
 
 export default routes;
