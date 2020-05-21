4304977958b234672e7c59e0a455ec41da928685
luanfonsecap <luanfonsecap@icloud.com>
2020-05-20 22:22:46 -0300

    adiciona schema de validação para SessionController

--- controller/SessionController.js
+++ controller/SessionController.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import * as Yup from "yup";
 
 import User from "../model/User";
 import authConfig from "../config/auth";
@@ -6,6 +7,18 @@ import authConfig from "../config/auth";
 class SessionController {
   async store(request, response) {
     const { email, password } = request.body;
+
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    schema.validate({ email, password }).catch(() => {
+      return response
+        .status(400)
+        .json({ error: "Invalid values to user or password" });
+    });
+
     const user = await User.findOne({ where: { email } });
 
     if (!user) return response.status(404).json({ error: "user not found" });
